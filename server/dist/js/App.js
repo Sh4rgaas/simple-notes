38381204b16f08dd8d2090097eaa03094918063d
Sh4rgaas <shloosh41@yahoo.com>
2022-06-29 10:52:23 +0300

    small fix for db connection, and addint client files

--- server/dist/js/App.js
+++ server/dist/js/App.js
@@ -11,7 +11,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4000;
 app.use((0, cors_1.default)());
 app.use(routes_1.default);
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clustertodo.raz9g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.jd58n.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 // const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose_1.default
     .connect(uri)
