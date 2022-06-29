bed74ee1f14798d56a6da8956ea5e2fb76cf8d9a
Sh4rgaas <shloosh41@yahoo.com>
2022-06-29 11:34:14 +0300

    added content to NoteItem.tsx

--- client/src/components/NoteItem.tsx
+++ client/src/components/NoteItem.tsx
@@ -1,1 +1,34 @@
-export {}
+import React from "react";
+
+type Props = NoteProps & {
+  updateNote: (note: INote) => void;
+  deleteNote: (_id: string) => void;
+}
+
+const Note: React.FC<Props> = ({ note, updateNote, deleteNote }) => {
+  const checkNote: string = note.status ? `line-through` : ""
+  return (
+    <div className="Card">
+      <div className="Card--text">
+        <h1 className={checkNote}>{note.name}</h1>
+        <span className={checkNote}>{note.description}</span>
+      </div>
+      <div className="Card--button">
+        <button
+          onClick={() => updateNote(note)}
+          className={note.status ? `hide-button` : "Card--button__done"}
+        >
+          Complete
+        </button>
+        <button
+          onClick={() => deleteNote(note._id)}
+          className="Card--button__delete"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Note;
